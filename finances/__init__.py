8ab7acd689378f88ae61b40648f38c88b620d988
Afonasev Eugene <1stlividar@gmail.com>
2014-05-14 19:54:17 +0600

    Add static files and main view

--- finances/__init__.py
+++ finances/__init__.py
@@ -1,28 +1,7 @@
 
-import bottle
+from .app import app
 
 
-index_html = 'My first! app! By {{ author }}'
+__version__ = '0.0.0'
 
-
-@bottle.route('/:anything')
-def something(anything=''):
-    return bottle.template(index_html, author=anything)
-
-
-@bottle.route('/')
-def index():
-    return bottle.template(index_html, author='your name here')
-
-
-@bottle.error(403)
-def mistake403(code):
-    return 'The parameter you passed has the wrong format!'
-
-
-@bottle.error(404)
-def mistake404(code):
-    return 'Sorry, this page does not exist!'
-
-
-app = bottle.default_app()
+__all__ = [app]
